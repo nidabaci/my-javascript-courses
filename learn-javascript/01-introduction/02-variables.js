d4cc6ed3f6fe0a63db9c33f1fab31489cb1cfd10
Jonida Baci <jonida.baci@hotmail.com>
2022-09-29 13:30:08 +0200

    file rename

--- learn-javascript/01-introduction/02-variables.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD:learn-javascript/01-introduction/02-variables.js
-// No code to show.
-=======
-// No code to show
->>>>>>> be394be6e560940032aa01311e742589f6b31c36:learn-javascript/introduction/02-variables.js
