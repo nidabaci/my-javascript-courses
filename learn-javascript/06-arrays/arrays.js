c962d7e6f406ec7614c21ee2b7d0c8972ba5509e
Jonida Baci <jonida.baci@hotmail.com>
2022-09-30 14:59:21 +0200

    feat: Created 'array' folder and added 'array' file

---
+++ learn-javascript/06-arrays/arrays.js
@@ -0,0 +1,3 @@
+// Arrays are lists of ordered, stored data. 
+// They can hold items that are of any data type. 
+// Arrays are created by using square brackets, with individual elements separated by commas.
