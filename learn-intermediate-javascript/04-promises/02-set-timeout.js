8fbdef9a608d40f7d70c76daca24147f77aca97a
Jonida Baci <jonida.baci@hotmail.com>
2022-10-05 13:09:45 +0200

    feat: Created set-timeout file and added its content

---
+++ learn-intermediate-javascript/04-promises/02-set-timeout.js
@@ -0,0 +1,10 @@
+console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+function usingSTO() {
+  console.log("This is whatever.");
+}
+
+setTimeout(usingSTO,3000 );
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
