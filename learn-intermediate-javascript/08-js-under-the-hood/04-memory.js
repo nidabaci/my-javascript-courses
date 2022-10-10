5807db6b6754c344fdfdd8a4e213d43e2e405f0d
Jonida Baci <jonida.baci@hotmail.com>
2022-10-10 17:12:14 +0200

    feat: Created 'under the hood' folder and added it files

---
+++ learn-intermediate-javascript/08-js-under-the-hood/04-memory.js
@@ -0,0 +1,10 @@
+// When you execute a program, 
+// variables and all of your objects need to be stored and accessed in memory. 
+
+// JavaScript has two data structures for memory:
+
+// The heap (provides dynamic memory allocation at runtime for data types that don’t have a fixed size)
+// The stack (used for static storage)
+// When you assign values to variables, 
+// the JavaScript engine is tasked with figuring out if the value is a primitive or a reference value. 
+// The outcome determines if the value is stored in the heap or the stack.
