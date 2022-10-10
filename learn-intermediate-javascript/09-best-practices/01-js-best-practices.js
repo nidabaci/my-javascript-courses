7c2179a9209a7a161633da9b1ebae609d21f09ad
Jonida Baci <jonida.baci@hotmail.com>
2022-10-10 17:17:29 +0200

    Created 'best practices' folder and added its file lesson.

---
+++ learn-intermediate-javascript/09-best-practices/01-js-best-practices.js
@@ -0,0 +1,17 @@
+// Overall, solutions that implement design patterns tend to be:
+
+// DRY (Don’t repeat yourself)
+// Modular
+// Reusable
+// Easier to maintain
+// Easier to discuss with peers at a high level (vs diving into implementation details)
+
+
+
+
+// In contrast to design patterns being best practices, anti-patterns describe ineffective solutions to problems that result in bad things happening, such as:
+
+// Namespace pollution — unexpected behavior from interactions from different clients
+// Increased code complexity
+// Code being difficult to understand and update
+// Difficulty with testing and debugging code
